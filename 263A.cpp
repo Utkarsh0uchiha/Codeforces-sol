4012672bf489ddd88815d352a93b68e90c52bb18
Utkarsh Raj <117010422+Utkarsh0uchiha@users.noreply.github.com>
2023-08-26 17:55:11 +0530

    Beautiful Matrix

---
+++ 263A.cpp
@@ -0,0 +1,33 @@
+    #include <iostream>
+     
+    using namespace std;
+     
+    int main()
+    {
+        int arr[5][5];
+        for(int i = 0; i < 5 ; i++){
+            for(int j = 0; j < 5; j++){
+                cin>>arr[i][j];
+                
+            }
+            
+        }
+        int r = 0,c = 0;
+        for(int i = 0; i < 5 ; i++){
+            for(int j = 0; j < 5; j++){
+                if(arr[i][j] == 1){
+                    r = j;
+                    c = i;
+                    break;
+                }
+                
+            }
+            
+            
+        }
+        if(r == 2 && c == 2) cout<<0;
+        else if(r == 2) cout<<abs(c - 2);
+        else if(c == 2) cout<<abs(r - 2);
+        else cout<<abs(c - 2) + abs(r - 2);
+        return 0;
+    }

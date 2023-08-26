3cf4b9ce13f5fd47fff56387ef40059730b555d9
Utkarsh Raj <117010422+Utkarsh0uchiha@users.noreply.github.com>
2023-08-26 18:15:54 +0530

    Beautiful Matrix

--- 263A.cpp
+++ 263A.cpp
@@ -4,7 +4,7 @@
      
     int main()
     { 
-        int arr[5][5];
+        int arr[5][5]; 
         for(int i = 0; i < 5 ; i++){
             for(int j = 0; j < 5; j++){
                 cin>>arr[i][j];

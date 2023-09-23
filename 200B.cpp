550d27275f8648926aa5022525090735b0a2bf7a
Utkarsh Raj <117010422+Utkarsh0uchiha@users.noreply.github.com>
2023-09-23 19:00:27 +0530

    `Drinks`

---
+++ 200B.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    int n, p[110];
+    cin>>n;
+    double ans = 0.0, sum = 0.0;
+    for(int i = 0; i < n; i++){
+        cin>>p[i];
+    }
+    
+    for(int i = 0; i < n ; i++){
+        sum = sum + p[i];
+    }
+    ans = sum / n;
+    cout<<fixed<<setprecision(12)<<ans<<endl;
+ 
+    return 0;
+}

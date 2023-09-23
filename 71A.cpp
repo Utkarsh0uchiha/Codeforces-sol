07baabd61dd6e55ca0d54c3abc7e44b44a8d6fd7
Utkarsh Raj <117010422+Utkarsh0uchiha@users.noreply.github.com>
2023-09-23 19:08:53 +0530

    `Way Too Long Words`

---
+++ 71A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    while(n--){
+        string s;
+        cin>>s;
+        int len = s.length();
+        if(len > 10){
+            cout<<s[0]<<len - 2<<s[len - 1]<<endl;
+        }
+        else{
+            cout<<s<<endl;
+        }
+    }
+}
